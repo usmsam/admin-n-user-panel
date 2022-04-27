11fa131d1d7c4c8ed78800246f0c53b917409aac
Sam developer <samandarusmanov1@gmail.com>
2022-04-27 18:28:37 +0500

    add actions folder

---
+++ src/components/actions/actions.js
@@ -0,0 +1,41 @@
+import { LOGIN, LOGOUT, ADD_COMMENT, VERIFY_COMMENT, TOGGLE_POPUP , VERIFY_CLEAR } from '../types/types'
+
+export function loginAction(login, password) {
+    return {
+        type: LOGIN,
+        payload: { login: login, pass: password }
+    }
+}
+
+
+export function togglePopupAction() {
+    return { type: TOGGLE_POPUP }
+}
+
+
+export function logoutAction() {
+    return { type: LOGOUT }
+}
+
+
+export function addCommentAction(loginText, passwordText) {
+    if (loginText.trim() !== '' && passwordText.trim() !== '') {
+        return {
+            type: ADD_COMMENT,
+            payload: { title: loginText, text: passwordText }
+        }
+    }
+}
+
+export function verifyCommentAction(id) {
+    return {
+        type: VERIFY_COMMENT,
+        payload: id
+    }
+}
+export function verifyClearAction(id) {
+    return {
+        type: VERIFY_CLEAR,
+        payload: id
+    }
+}
