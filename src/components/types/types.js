db5c26a0dc918aa4d2f1ec059b9652b9a637fc2f
Sam developer <samandarusmanov1@gmail.com>
2022-04-27 18:27:07 +0500

    add types folder

---
+++ src/components/types/types.js
@@ -0,0 +1,10 @@
+export const VERIFY_COMMENT = 'VERIFY_COMMENT';
+export const VERIFY_CLEAR = 'VERIFY_CLEAR';
+export const ADD_COMMENT = 'ADD_COMMENT';
+
+
+export const TOGGLE_POPUP = 'TOGGLE_POPUP';
+
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
