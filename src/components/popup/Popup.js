e070d715c8e23449be339d2664f631619f856a91
Sam developer <samandarusmanov1@gmail.com>
2022-04-27 18:27:40 +0500

    add popup folder

---
+++ src/components/popup/Popup.js
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react'
+
+import { useDispatch } from 'react-redux'
+import { loginAction } from '../actions/actions'
+import Error from '../errorBlock/Error'
+
+const Popup = () => {
+    const dispatch = useDispatch()
+    const [loginText, setLoginText] = useState('')
+    const [passwordText, setPasswordText] = useState('')
+
+    return (
+        <div className='popup'>
+            <Error />
+
+            <input
+                placeholder='login...'
+                value={loginText}
+                onChange={(e) => setLoginText(e.target.value)}
+            />
+            <input
+                placeholder='Password...'
+                value={passwordText}
+                onChange={(e) => setPasswordText(e.target.value)}
+
+            />
+
+            <button
+                onClick={() => dispatch(loginAction(loginText, passwordText))}
+            >
+                Sign in
+            </button>
+
+        </div >
+    )
+}
+
+export default Popup
